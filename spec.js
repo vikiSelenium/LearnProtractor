0b686dc56a1800fb470fab97e24402faeaa72da4
vikiSelenium <vikivijay7@gmail.com>
2020-03-07 12:55:21 +0530

    my First Commit Protractor

---
+++ spec.js
@@ -0,0 +1,26 @@
+
+describe('Enter name feature', function()
+{
+    it('should enter name as Tom', function()
+    {
+        browser.get('https://angularjs.org/');
+        element(by.model('yourName')).sendKeys('Tom');
+        browser.driver.sleep(3000);
+        
+});
+
+});
+
+describe('multiple Elements', function()
+{
+    it('should add one and two', function()
+    {
+browser.get('http://juliemr.github.io/protractor-demo');
+element(by.model('first')).sendKeys('1');
+element(by.model('second')).sendKeys('2');
+
+element(by.id('gobutton')).click();
+expect(element(by.className('ng-binding')).getText()).toEqual('3');
+
+    });
+});
---
+++ specs.js
@@ -0,0 +1,37 @@
+
+describe('Multiple scenarios', function()
+{
+    var firstNum = element(by.model('first'));
+    var secondNum = element(by.model('second'));
+    var clickButton = element(by.id('gobutton'));
+    var result = element(by.className('ng-binding'));
+    beforeEach(function(){
+        browser.get('http://juliemr.github.io/protractor-demo');
+    });
+   //tc1
+    it('We are running multiple scenarios', function(){
+
+        expect(browser.getTitle()).toEqual('Super Calculator');
+
+        });
+        //tc2 
+        it('should add two variables', function()
+        {
+            firstNum.sendKeys('1');
+            secondNum.sendKeys('2');
+            clickButton.click();
+
+            expect(result.getText()).toEqual('3');
+       });
+        //tc3
+        it('should add 4 and 6', function()
+        {
+            firstNum.sendKeys('4');
+            secondNum.sendKeys('6');
+            clickButton.click();
+
+            expect(result.getText()).toEqual('10');
+
+        });
+    });
+
